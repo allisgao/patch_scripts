1e7081f18ddd61eaaee2977472d50ec43feb4a33
allisgao <gaojz017@163.com>
2017-08-31 20:01:57 +0800

    modify the paras in PATCH_ENV, which affected basic_patch_scripts.sh file.

--- basic_patch_scripts.sh
+++ basic_patch_scripts.sh
@@ -10,11 +10,11 @@ FILENAME=$BASH_SOURCE
 PATCH_ID=`echo $FILENAME | awk -F. '{print$1}'`
 PATCH_DRV=u${PATCH_ID}.drv
 source ./PATCH_ENV
-cd $IE_PATCHES/$PATCH_ID
+cd $PATCH_HOME/$PATCH_ID
 echo -e "\033[42;30m $PATCH_ID is patching... \033[0m"
 admsi.pl && adpatch driver=$PATCH_DRV 
 if [ -d "${PATCH_ID}_ZHS"];then
-	cd $IE_PATCHES/${PATCH_ID}_ZHS
+	cd $PATCH_HOME/${PATCH_ID}_ZHS
 	echo -e "\033[42;30m ${PATCH_ID}_ZHS is patching... \033[0m"
 	adpatch driver=$PATCH_DRV
 else
