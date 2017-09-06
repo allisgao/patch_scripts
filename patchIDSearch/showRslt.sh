8db3f6d73efb44f74caa64de61d2a042af19f4ad
allisgao <gaojz017@163.com>
2017-09-06 14:42:18 +0800

    modify showReslt.sh

--- patchIDSearch/showRslt.sh
+++ patchIDSearch/showRslt.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-
-# delete old result_files
-rm -rf result/*
-# create a result_file for each patch named $pid
-for pid in `cat patchID|grep '^[0-9]'`
-do
-	echo 'chcking '$pid ', please wait...'
-	sh searchPID.sh $pid > results/$pid
-	echo 'checking '$pid' complete.'
-done
-# change dir to /results
-cd  results/
-for rfile in `ls ./`
-do 
-	if [ `wc -l $rfile|awk '{print $1}'` == '16'  ] ;then 
-		echo $rfile is not patched.
-	fi
-done
+if $1=='';then
+	# delete old result_files
+	rm -rf result/*
+	# create a result_file for each patch named $pid
+	for pid in `cat patchID|grep '^[0-9]'`
+	do
+		echo 'chcking '$pid ', please wait...'
+		sh searchPID.sh $pid > results/$pid
+		echo 'checking '$pid' complete.'
+	done
+	# change dir to /results
+	cd  results/
+	for rfile in `ls ./`
+	do 
+		if [ `wc -l $rfile|awk '{print $1}'` == '16'  ] ;then 
+			echo $rfile is not patched.
+		fi
+	done
+else
+	opatch lsinventory|grep $1
